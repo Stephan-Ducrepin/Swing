fc8061671925225ff860ecc7ce11f33512187aaa
EC2 Default User <ec2-user@ip-172-31-28-144.us-east-2.compute.internal>
2019-07-30 13:50:23 +0000

    This is the MVP

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,26 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template('index.html')
+    
+@app.route('/immigration')
+def immigration():
+    return render_template('landing.html')
+    
+@app.route('/survey')
+def survey():
+    return render_template('iSurvey.html')
+    
+@app.route('/results', methods = ['GET', 'POST'])
+def results():
+    if request.method == 'GET':
+        return render_template('iSurvey.html')
+    else:
+        value = int(request.form['istance'])
+        print (value)
+        if value >= 5:
+            return render_template('otherResults.html')
+        else:
+            return render_template('results.html', value=value)
+    
+    
